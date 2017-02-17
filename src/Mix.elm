2ea39ec833c0b87f22b00ac8e9a9535c1a37f5b9
Daniel Barter <danielbarter@gmail.com>
2017-02-17 11:52:15 -0500

    finished StoreA

--- src/Mix.elm
+++ src/Mix.elm
@@ -22,6 +22,16 @@ type alias BigRegister = ( Sign
                          , Byte
                          )
 
+flipSignBig : BigRegister -> BigRegister
+flipSignBig (s,b1,b2,b3,b4,b5) = case s of
+                                     Pos -> (Neg,b1,b2,b3,b4,b5)
+                                     Neg -> (Pos,b1,b2,b3,b4,b5)
+
+flipSignSmall : SmallRegister -> SmallRegister
+flipSignSmall (s,b1,b2) = case s of
+                              Pos -> (Neg,b1,b2)
+                              Neg -> (Pos,b1,b2)
+    
 type alias Address = Int
 type alias Index = Int
 type alias Modification = Int
@@ -115,6 +125,39 @@ maskSmall l r (s,b1,b2,b3,b4,b5) =
            (4,5) -> Ok (Pos,b4,b5)
            (5,5) -> Ok (Pos,zero,b5)
            _ -> Err <| InvalidMask l r
+
+
+storeBig : LeftMask
+         -> RightMask
+         -> BigRegister
+         -> BigRegister -> Result RuntimeError BigRegister
+
+storeBig l r (msn,m1,m2,m3,m4,m5) (asn,a1,a2,a3,a4,a5)
+    = case (l,r) of
+          (0,0) -> Ok (asn,m1,m2,m3,m4,m5)
+          (0,1) -> Ok (asn,a5,m2,m3,m4,m5)
+          (0,2) -> Ok (asn,a4,a5,m3,m4,m5)
+          (0,3) -> Ok (asn,a3,a4,a5,m4,m5)
+          (0,4) -> Ok (asn,a2,a3,a4,a5,m5)
+          (0,5) -> Ok (asn,a1,a2,a3,a4,a5)
+          (1,1) -> Ok (msn,a5,m2,m3,m4,m5)
+          (1,2) -> Ok (msn,a4,a5,m3,m4,m5)
+          (1,3) -> Ok (msn,a3,a4,a5,m4,m5)
+          (1,4) -> Ok (msn,a2,a3,a4,a5,m5)
+          (1,5) -> Ok (msn,a1,a2,a3,a4,a5)
+          (2,2) -> Ok (msn,m1,a5,m3,m4,m5)
+          (2,3) -> Ok (msn,m1,a4,a5,m4,m5)
+          (2,4) -> Ok (msn,m1,a3,a4,a5,m5)
+          (2,5) -> Ok (msn,m1,a2,a3,a4,a5)
+          (3,3) -> Ok (msn,m1,m2,a5,m4,m5)
+          (3,4) -> Ok (msn,m1,m2,a4,a5,m5)
+          (3,5) -> Ok (msn,m1,m2,a3,a4,a5)
+          (4,4) -> Ok (msn,m1,m2,m3,a5,m5)
+          (4,5) -> Ok (msn,m1,m2,m3,a4,a5)
+          (5,5) -> Ok (msn,m1,m2,m3,m4,a5)
+          _ -> Err <| InvalidMask l r
+
+
                 
 type alias Memory = Dict.Dict Address BigRegister
 
@@ -161,7 +204,20 @@ when adding an instruction, you need to update
 type Instruction = LoadA Address Index LeftMask RightMask
                  | LoadX Address Index LeftMask RightMask
                  | LoadI1 Address Index LeftMask RightMask
-
+                 | LoadI2 Address Index LeftMask RightMask
+                 | LoadI3 Address Index LeftMask RightMask
+                 | LoadI4 Address Index LeftMask RightMask
+                 | LoadI5 Address Index LeftMask RightMask
+                 | LoadI6 Address Index LeftMask RightMask
+                 | LoadANeg Address Index LeftMask RightMask
+                 | LoadXNeg Address Index LeftMask RightMask
+                 | LoadI1Neg Address Index LeftMask RightMask
+                 | LoadI2Neg Address Index LeftMask RightMask
+                 | LoadI3Neg Address Index LeftMask RightMask
+                 | LoadI4Neg Address Index LeftMask RightMask
+                 | LoadI5Neg Address Index LeftMask RightMask
+                 | LoadI6Neg Address Index LeftMask RightMask
+                 | StoreA Address Index LeftMask RightMask
 
 step : Mix -> Result RuntimeError Mix
 step s = case getInstruction s of
@@ -187,6 +243,48 @@ decodeInstruction r =
         (a,i,m,9) -> case getMasks m of
                          Err err  -> Err err
                          Ok (l,r) -> Ok <| LoadI1 a i l r
+        (a,i,m,10) -> case getMasks m of
+                         Err err  -> Err err
+                         Ok (l,r) -> Ok <| LoadI2 a i l r 
+        (a,i,m,11) -> case getMasks m of
+                         Err err  -> Err err
+                         Ok (l,r) -> Ok <| LoadI3 a i l r
+        (a,i,m,12) -> case getMasks m of
+                         Err err  -> Err err
+                         Ok (l,r) -> Ok <| LoadI4 a i l r
+        (a,i,m,13) -> case getMasks m of
+                         Err err  -> Err err
+                         Ok (l,r) -> Ok <| LoadI5 a i l r
+        (a,i,m,14) -> case getMasks m of
+                         Err err  -> Err err
+                         Ok (l,r) -> Ok <| LoadI6 a i l r
+        (a,i,m,16) -> case getMasks m of
+                         Err err  -> Err err
+                         Ok (l,r) -> Ok <| LoadANeg a i l r
+        (a,i,m,23) -> case getMasks m of
+                          Err err  -> Err err
+                          Ok (l,r) -> Ok <| LoadXNeg a i l r
+        (a,i,m,17) -> case getMasks m of
+                         Err err  -> Err err
+                         Ok (l,r) -> Ok <| LoadI1Neg a i l r
+        (a,i,m,18) -> case getMasks m of
+                         Err err  -> Err err
+                         Ok (l,r) -> Ok <| LoadI2Neg a i l r 
+        (a,i,m,19) -> case getMasks m of
+                         Err err  -> Err err
+                         Ok (l,r) -> Ok <| LoadI3Neg a i l r
+        (a,i,m,20) -> case getMasks m of
+                         Err err  -> Err err
+                         Ok (l,r) -> Ok <| LoadI4Neg a i l r
+        (a,i,m,21) -> case getMasks m of
+                         Err err  -> Err err
+                         Ok (l,r) -> Ok <| LoadI5Neg a i l r
+        (a,i,m,22) -> case getMasks m of
+                         Err err  -> Err err
+                         Ok (l,r) -> Ok <| LoadI6Neg a i l r
+        (a,i,m,24) -> case getMasks m of
+                          Err err  -> Err err
+                          Ok (l,r) -> Ok <| StoreA a i l r
         (_,_,_,c) -> Err <| UnrecognizedInstructionCode c
         
 
@@ -220,8 +318,140 @@ executeInstruction inst s =
                                                          Ok newI1 -> Ok { s
                                                                         | i1 = newI1
                                                                         }
+        LoadI2 a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                              Err err -> Err err
+                              Ok v -> case Dict.get (a+v) s.mem of
+                                          Nothing -> Err <| NoMemoryValue (a+v)
+                                          Just br  -> case maskSmall l r br of
+                                                         Err err -> Err err
+                                                         Ok newI2 -> Ok { s
+                                                                        | i2 = newI2
+                                                                        }
+        LoadI3 a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                              Err err -> Err err
+                              Ok v -> case Dict.get (a+v) s.mem of
+                                          Nothing -> Err <| NoMemoryValue (a+v)
+                                          Just br  -> case maskSmall l r br of
+                                                         Err err -> Err err
+                                                         Ok newI3 -> Ok { s
+                                                                        | i3 = newI3
+                                                                        }
+        LoadI4 a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                              Err err -> Err err
+                              Ok v -> case Dict.get (a+v) s.mem of
+                                          Nothing -> Err <| NoMemoryValue (a+v)
+                                          Just br  -> case maskSmall l r br of
+                                                         Err err -> Err err
+                                                         Ok newI4 -> Ok { s
+                                                                        | i4 = newI4
+                                                                        }
+        LoadI5 a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                              Err err -> Err err
+                              Ok v -> case Dict.get (a+v) s.mem of
+                                          Nothing -> Err <| NoMemoryValue (a+v)
+                                          Just br  -> case maskSmall l r br of
+                                                         Err err -> Err err
+                                                         Ok newI5 -> Ok { s
+                                                                        | i5 = newI5
+                                                                        }
+        LoadI6 a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                              Err err -> Err err
+                              Ok v -> case Dict.get (a+v) s.mem of
+                                          Nothing -> Err <| NoMemoryValue (a+v)
+                                          Just br  -> case maskSmall l r br of
+                                                         Err err -> Err err
+                                                         Ok newI6 -> Ok { s
+                                                                        | i6 = newI6
+                                                                        }
+
+        LoadANeg a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                                Err err -> Err err
+                                Ok v -> case Dict.get (a+v) s.mem of
+                                            Nothing -> Err <| NoMemoryValue (a+v)
+                                            Just br -> case mask l r br of
+                                                           Err err -> Err err
+                                                           Ok newA -> Ok { s
+                                                                         | a = flipSignBig newA
+                                                                         }
+        LoadXNeg a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                                Err err -> Err err
+                                Ok v -> case Dict.get (a+v) s.mem of
+                                            Nothing -> Err <| NoMemoryValue (a+v)
+                                            Just br  -> case mask l r br of
+                                                           Err err -> Err err
+                                                           Ok newX -> Ok { s
+                                                                         | x = flipSignBig newX
+                                                                         }
+        LoadI1Neg a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                                 Err err -> Err err
+                                 Ok v -> case Dict.get (a+v) s.mem of
+                                             Nothing -> Err <| NoMemoryValue (a+v)
+                                             Just br  -> case maskSmall l r br of
+                                                            Err err -> Err err
+                                                            Ok newI1 -> Ok { s
+                                                                           | i1 = flipSignSmall newI1
+                                                                           }
+        LoadI2Neg a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                                 Err err -> Err err
+                                 Ok v -> case Dict.get (a+v) s.mem of
+                                             Nothing -> Err <| NoMemoryValue (a+v)
+                                             Just br  -> case maskSmall l r br of
+                                                            Err err -> Err err
+                                                            Ok newI2 -> Ok { s
+                                                                           | i2 = flipSignSmall newI2
+                                                                           }
+        LoadI3Neg a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                                 Err err -> Err err
+                                 Ok v -> case Dict.get (a+v) s.mem of
+                                             Nothing -> Err <| NoMemoryValue (a+v)
+                                             Just br  -> case maskSmall l r br of
+                                                            Err err -> Err err
+                                                            Ok newI3 -> Ok { s
+                                                                           | i3 = flipSignSmall newI3
+                                                                           }
+        LoadI4Neg a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                                 Err err -> Err err
+                                 Ok v -> case Dict.get (a+v) s.mem of
+                                             Nothing -> Err <| NoMemoryValue (a+v)
+                                             Just br  -> case maskSmall l r br of
+                                                            Err err -> Err err
+                                                            Ok newI4 -> Ok { s
+                                                                           | i4 = flipSignSmall newI4
+                                                                           }
+        LoadI5Neg a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                                 Err err -> Err err
+                                 Ok v -> case Dict.get (a+v) s.mem of
+                                             Nothing -> Err <| NoMemoryValue (a+v)
+                                             Just br  -> case maskSmall l r br of
+                                                            Err err -> Err err
+                                                            Ok newI5 -> Ok { s
+                                                                           | i5 = flipSignSmall newI5
+                                                                           }
+        LoadI6Neg a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                                 Err err -> Err err
+                                 Ok v -> case Dict.get (a+v) s.mem of
+                                             Nothing -> Err <| NoMemoryValue (a+v)
+                                             Just br  -> case maskSmall l r br of
+                                                            Err err -> Err err
+                                                            Ok newI6 -> Ok { s
+                                                                           | i6 = flipSignSmall newI6
+                                                                           }
+        StoreA a i l r -> case Result.map readSmall <| getIndexRegister i s of
+                              Err err -> Err err
+                              Ok v -> case Dict.get (a+v) s.mem of
+                                          Nothing -> case (storeBig l r (Pos,byte 0,byte 0,byte 0,byte 0,byte 0) s.a) of
+                                                         Err err -> Err err
+                                                         Ok mm -> Ok { s
+                                                                     | mem = Dict.insert (a+v) mm s.mem
+                                                                     }
+                                          Just m -> case (storeBig l r m s.a) of
+                                                        Err err -> Err err
+                                                        Ok mm -> Ok { s
+                                                                    | mem = Dict.insert (a+v) mm s.mem
+                                                                    }
 
 
+                                                                   
 getIndexRegister : Index -> Mix -> Result RuntimeError SmallRegister
 getIndexRegister i m =
     case i of
@@ -299,3 +529,45 @@ testLoadI1 = let br = (Pos,byte 0,byte 0,byte 0,byte 0,byte 0)
                 , comparison = E
                 }
 
+testLoadI6 : Mix
+testLoadI6 = let br = (Pos,byte 0,byte 0,byte 0,byte 0,byte 0)
+                 sr = (Pos, byte 0, byte 0)
+                 m = Dict.fromList [ (0,(Pos,byte 20,byte 0,byte 3,byte 13,byte 14))
+                                   , (1899,(Neg,byte 0,byte 80,byte 3,byte 5,byte 4))
+                                   ]
+             in { a = br
+                , x = br
+                , i1 = sr
+                , i2 = sr
+                , i3 = (Neg,byte 01,byte 01)
+                , i4 = sr
+                , i5 = sr
+                , i6 = sr
+                , j = sr
+                , p = 0
+                , mem = m
+                , overflow = Good
+                , comparison = E
+                }
+
+
+testStoreA : Mix
+testStoreA = let br = (Pos,byte 0,byte 0,byte 0,byte 0,byte 0)
+                 sr = (Pos, byte 0, byte 0)
+                 m = Dict.fromList [ (0,(Pos,byte 20,byte 0,byte 3,byte 19,byte 24))
+                                   , (1899,(Neg,byte 1,byte 2,byte 3,byte 4,byte 5))
+                                   ]
+             in { a = (Pos,byte 6,byte 7,byte 8,byte 9,byte 0)
+                , x = br
+                , i1 = sr
+                , i2 = sr
+                , i3 = (Neg,byte 01,byte 01)
+                , i4 = sr
+                , i5 = sr
+                , i6 = sr
+                , j = sr
+                , p = 0
+                , mem = m
+                , overflow = Good
+                , comparison = E
+                }
